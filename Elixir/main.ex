6456a1330ddafe7f4bff5f0af9679d660e1e5df9
Reese Letts <111404863+rkletts@users.noreply.github.com>
2025-05-03 16:37:03 -0400

    Create main.ex

---
+++ Elixir/main.ex
@@ -0,0 +1,15 @@
+defmodule Main do
+  def run do
+    a1 = Dog.start_link("Rex")
+    a2 = Cat.start_link("Whiskers")
+
+    send(a1, {:speak, self()})
+    send(a2, {:speak, self()})
+
+    # Optionally stop the processes
+    send(a1, :stop)
+    send(a2, :stop)
+  end
+end
+
+Main.run()
