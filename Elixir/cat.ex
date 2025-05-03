f16709bb9e3b3625ad20adaa597f5d2c85050d4f
Reese Letts <111404863+rkletts@users.noreply.github.com>
2025-05-03 16:36:42 -0400

    Create cat.ex

---
+++ Elixir/cat.ex
@@ -0,0 +1,23 @@
+defmodule Cat do
+  def start_link(name) do
+    spawn_link(fn -> loop(name) end)
+  end
+
+  defp loop(name) do
+    receive do
+      {:get_name, caller} ->
+        send(caller, {:name, name})
+        loop(name)
+
+      {:speak, _caller} ->
+        IO.puts("#{name} says: Meow!")
+        loop(name)
+
+      :stop ->
+        :ok
+
+      _ ->
+        loop(name)
+    end
+  end
+end
